e856e289a3c3bc457e7a5100d896b8cbb2ac0657
Austin Densley <austin.densley00@gmail.com>
2025-01-26 00:02:26 -0700

    final

--- obj/Debug/net8.0-windows/RockPaperScissorsGUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RockPaperScissorsGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperScissorsGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b803a0cc5dc38b02bb473f319deb54f2953f4dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4705a381071353bb9995ee44e90ada538861cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperScissorsGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperScissorsGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
